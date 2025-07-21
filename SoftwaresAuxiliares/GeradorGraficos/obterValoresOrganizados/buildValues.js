ac58b8f1b0f40b1cb9809f60546eea627c812bb5
Gabriela <gabrielamalvar11@gmail.com>
2025-07-20 23:05:57 -0300

    Correção valores de referencia métricas de coesão. Correção de todos os gráficos

--- SoftwaresAuxiliares/GeradorGraficos/obterValoresOrganizados/buildValues.js
+++ SoftwaresAuxiliares/GeradorGraficos/obterValoresOrganizados/buildValues.js
@@ -1,139 +1,3 @@
-
-// --------------------------------------------------------
-// GERA RESULTADO PARA LATEX
-// --------------------------------------------------------
-
-// const fs = require('fs');
-// const path = require('path');
-// const { parse } = require('csv-parse/sync');
-
-// // Função para processar o CSV e gerar LaTeX
-// function processCSV(filePath) {
-//     const content = fs.readFileSync(filePath, 'utf-8');
-//     const records = parse(content, { columns: true, skip_empty_lines: true });
-
-//     // Identificar colunas numéricas
-//     const numericColumn = Object.keys(records[0]).filter(field =>
-//         records.some(record => record[field] !== '')
-//     );
-
-//     // Tabelas de referência
-//     const ref = {
-//         // Métricas discretas
-//         assignmentsQty: { bom: 5, ruim: 21 },
-//         cbo: { bom: 10, ruim: 22 },
-//         dit: { bom: 2, ruim: 3 },
-//         fanin: { bom: 2, ruim: 7 },
-//         fanout: { bom: 7, ruim: 15 },
-//         finalFieldsQty: { bom: 1, ruim: 3 },
-//         loc: { bom: 36, ruim: 114 },
-//         maxNestedBlocksQty: { bom: 1, ruim: 3 },
-//         modifiers: { bom: 2, ruim: 17 },
-//         nosi: { bom: 2, ruim: 9 },
-//         numbersQty: { bom: 1, ruim: 8 },
-//         privateFieldsQty: { bom: 1, ruim: 4 },
-//         publicMethodsQty: { bom: 3, ruim: 9 },
-//         returnQty: { bom: 2, ruim: 8 },
-//         rfc: { bom: 9, ruim: 29 },
-//         stringLiteralsQty: { bom: 3, ruim: 15 },
-//         totalFieldsQty: { bom: 2, ruim: 5 },
-//         totalMethodsQty: { bom: 5, ruim: 12 },
-//         uniqueWordsQty: { bom: 43, ruim: 96 },
-//         variablesQty: { bom: 5, ruim: 19 },
-//         wmc: { bom: 7, ruim: 22 },
-//         // Métricas Discretas sem o valor 0.
-//         abstractMethodsQty: { bom: 3, ruim: 6 },
-//         anonymousClassesQty: { bom: 2, ruim: 5 },
-//         comparisonsQty: { bom: 5, ruim: 13 },
-//         defaultFieldsQty: { bom: 3, ruim: 5 },
-//         defaultMethodsQty: { bom: 2, ruim: 6 },
-//         finalMethodsQty: { bom: 3, ruim: 7 },
-//         innerClassesQty: { bom: 2, ruim: 4 },
-//         lambdasQty: { bom: 3, ruim: 8 },
-//         logStatementsQty: { bom: 4, ruim: 9 },
-//         loopQty: { bom: 3, ruim: 8 },
-//         mathOperationsQty: { bom: 4, ruim: 11 },
-//         noc: { bom: 4, ruim: 9 },
-//         parenthesizedExpsQty: { bom: 3, ruim: 8 },
-//         privateMethodsQty: { bom: 3, ruim: 6 },
-//         protectedFieldsQty: { bom: 3, ruim: 7 },
-//         protectedMethodsQty: { bom: 2, ruim: 5 },
-//         publicFieldsQty: { bom: 2, ruim: 7 },
-//         staticFieldsQty: { bom: 2, ruim: 6 },
-//         staticMethodsQty: { bom: 2, ruim: 6 },
-//         synchronizedFieldsQty: { bom: 0, ruim: 0 },
-//         synchronizedMethodsQty: { bom: 3, ruim: 6 },
-//         tryCatchQty: { bom: 2, ruim: 6 },
-//         //Métricas contínuas
-//         lcc: { bom: 0.6044, ruim: 1 },
-//         lcom: { bom: 0.4, ruim: 0.7917 },
-//         tcc: { bom: 0.4483, ruim: 1 }
-//     };
-
-//     // Classificar os dados com base na tabela de referência
-//     function classify(metric, value) {
-//         const refValue = ref[metric];
-//         if (!refValue) return 'N/A';
-
-//         if (metric === 'lcc' || metric === 'lcom' || metric === 'tcc') {
-//             if (value < refValue.bom) return 'Bom';
-//             if (value < refValue.ruim) return 'Regular';
-//             return 'Ruim';
-//         }
-
-//         if (!refValue) return 'N/A';
-//         if (value <= refValue.bom) return 'Bom';
-//         if (value <= refValue.ruim) return 'Regular';
-//         return 'Ruim';
-//     }
-
-//     let result = {};
-//     numericColumn.forEach(column => {
-//         result[column] = { Bom: 0, Regular: 0, Ruim: 0, Total: 0 };
-//     });
-
-//     records.forEach(data => {
-//         numericColumn.forEach(column => {
-//             const value = parseFloat(data[column]);
-//             if (!isNaN(value)) {
-//                 const classificacao = classify(column, value);
-//                 if (classificacao !== 'N/A') {
-//                     result[column][classificacao]++;
-//                     result[column].Total++;
-//                 }
-//             }
-//         });
-//     });
-
-//     // Gerar tabela LaTeX
-//     let latex = `\\begin{tabular}{lccc}
-//                 \\toprule
-//                 \\textbf{Métrica} & \\textbf{Bom (\\%)} & \\textbf{Regular (\\%)} & \\textbf{Ruim (\\%)} \\\\
-//                 \\midrule
-//                 `;
-//     numericColumn.forEach(column => {
-//         const total = result[column].Total;
-//         if (total > 0) {
-//             const bom = ((result[column].Bom / total) * 100).toFixed(1);
-//             const regular = ((result[column].Regular / total) * 100).toFixed(1);
-//             const ruim = ((result[column].Ruim / total) * 100).toFixed(1);
-//             latex += `${column} & ${bom} & ${regular} & ${ruim} \\\\\n`;
-//         }
-//     });
-
-//     latex += `\\bottomrule
-//               \\end{tabular}
-//             `;
-
-//     fs.writeFileSync('tabelaResultados.tex', latex, 'utf-8');
-//     console.log('Tabela LaTeX gerada em tabelaResultados.tex');
-// }
-
-// const filePath = path.join(__dirname, 'class.csv');
-// processCSV(filePath);
-
-
-
 // --------------------------------------------------------
 // GERA RESULTADO PARA GRÁFICOS
 // --------------------------------------------------------
@@ -199,9 +63,9 @@ function processCSV(filePath) {
         synchronizedMethodsQty: { bom: 3, ruim: 6 },
         tryCatchQty: { bom: 2, ruim: 6 },
         //Métricas contínuas
-        lcc: { bom: 0.6044, ruim: 1 },
+        lcc: { bom: 1, ruim: 0.6044 },
         lcom: { bom: 0.4, ruim: 0.7917 },
-        tcc: { bom: 0.4483, ruim: 1 }
+        tcc: { bom: 1, ruim: 0.4483 }
     };
 
     // Classificar os dados com base na tabela de referência
@@ -209,9 +73,15 @@ function processCSV(filePath) {
         const refValue = ref[metric];
         if (!refValue) return 'N/A';
 
-        if (metric === 'lcc' || metric === 'lcom' || metric === 'tcc') {
+        if (metric === 'lcom') {
             if (value < refValue.bom) return 'Bom';
             if (value < refValue.ruim) return 'Regular';
+            return 'Ruim';
+        }
+
+        if (metric === 'lcc' || metric === 'tcc') {
+            if (value === refValue.bom) return 'Bom';
+            if (value >= refValue.ruim) return 'Regular';
             return 'Ruim';
         }
 
